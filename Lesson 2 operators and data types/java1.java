5264b3d14996e00d3fdd3bb0f80019e6524f0346
TTRT-dot <xdamer6469@gmail.com>
2025-06-03 11:31:31 +0530

    xaxs

---
+++ Lesson 2 operators and data types/java1.java
@@ -0,0 +1,24 @@
+class java1{
+    public static void main(String[] args) {
+        int a=10;
+        int b=5;
+        int sum=a+b;
+        int diff=a-b;
+        int mult=a*b;
+        int div=a/b;
+        String magic="============================Magic===============================";
+        System.out.println("==========================================================Method 1=====================================================================");
+        System.out.println("A+B="+sum);
+        System.out.println("A-B="+diff);
+        System.out.println("A*B="+mult);
+        System.out.println("A/B="+div);
+        System.out.println("==========================================================Method 2=====================================================================");
+        System.out.println("A+B="+(a+b));
+        System.out.println("A-B="+(a-b));
+        System.out.println("A*B="+(a*b));
+        System.out.println("A/B="+(a/b));
+        System.out.println("remainder of A/B"+(a%b));
+        System.out.println(magic);
+        System.out.println("Addition = "+(a+b)+" Substraction = "+(a-b)+" Multiplication = "+(a*b)+" Division = "+(a/b));
+    }
+}
