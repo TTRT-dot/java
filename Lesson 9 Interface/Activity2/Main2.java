cee7079831e4ecc2e033d78e69f89d8c53de09fb
TTRT-dot <xdamer6469@gmail.com>
2025-06-22 12:16:10 +0530

    asd

--- Lesson 9 Interface/Activity2/Main2.java
+++ Lesson 9 Interface/Activity2/Main2.java
@@ -1,5 +1,77 @@
-package Lesson 9 Interface;
-
-public class Main2 {
+interface vehicle{
+        //all are the abstract methods.
+        void changegear(int,a);
+        void speedup(int,a);
+        void applybrakes(int,a);
+}
+//we use keyword implements to inherit a interface
+class Bicycle implements vehicle{
+    int speed;
+    int gear;
     
+    //to change gear
+    @Override
+    public void changegear(int newgear){
+        gear = newgear;
+    }
+
+    //to increse speed
+    @Override
+    public void speedup(int increment){
+        speed=speed+increment;
+    }
+    
+    //to decrease speed
+    @Override
+    public void applybrakes(int decrement){
+        speed = speed - decrement;
+    }
+    public void printStates("Speed = "+speed+"Gear = "+gear);
+}
+
+class car implements vehicle{
+    // to change gear
+    @Override
+    public void changegear(int newgear) {
+        gear = newgear;
+    }
+
+    // to increse speed
+    @Override
+    public void speedup(int increment) {
+        speed = speed + increment;
+    }
+
+    // to decrease speed
+    @Override
+        public void applybrakes(int decrement){
+            speed = speed - decrement;
+        }
+
+    public void printStates("Speed = "+speed+"Gear = "+gear);
+}
+
+class Main2 {
+      
+    public static void main (String[] args) {
+      
+        // creating an inatance of Bicycle 
+        // doing some operations 
+        Bicycle bicycle = new Bicycle();
+        bicycle.changegear(2);
+        bicycle.speedup(3);
+        bicycle.applybrakes(1);
+          
+        System.out.println("Bicycle present state :");
+        bicycle.printStates();
+          
+        // creating instance of the car.
+        Car car = new Car();
+        car.changegear(1);
+        car.speedup(4);
+        car.applybrakes(3);
+          
+        System.out.println("Car present state :");
+        car.printStates();
+    }
 }
